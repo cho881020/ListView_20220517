e9949219714184c8bb87c8c23f64c29c633c051f
조경진 <cho881020@gmail.com>
2022-05-17 13:45:57 +0900

    실제 데이터 반영하기  - 뿌려줄 데이터 가져오기  - 반영할 태그들 코딩으로 가져오기  - 실제 데이터 적용

--- app/src/main/java/kr/nepp/listview_20220517/adapters/StudentAdapter.kt
+++ app/src/main/java/kr/nepp/listview_20220517/adapters/StudentAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import kr.nepp.listview_20220517.R
 import kr.nepp.listview_20220517.datas.StudentData
 
@@ -26,7 +27,25 @@ class StudentAdapter(
 
         val row = tempRow!!  // row가 리스트뷰의 한 줄에 해당하는 그림 (View 타입).
 
+//        화면에 뿌려지기 전에, row 내부를 수정하자.
 
+//        데이터 / 반영 UI 태그 준비.
+
+        val data = mList[position]
+
+        val txtName = row.findViewById<TextView>(R.id.txtName)
+        val txtAge = row.findViewById<TextView>(R.id.txtAge)
+        val txtAddress = row.findViewById<TextView>(R.id.txtAddress)
+
+//        실제 연결
+
+        txtName.text = data.name
+        txtAddress.text = data.address
+
+        txtAge.text = "${2022 - data.birthYear + 1}세"
+
+
+//        만들어진 row의 그림이 리스트뷰에 뿌려짐.
         return row
     }
 
